7d96dc01210ea5cbd52b48779667a0531b47b04c
KiraThuta <kelvingarrat20320@gmail.com>
2024-01-31 20:19:17 +0630

    add all models

---
+++ src/main/java/Models/Record.java
@@ -0,0 +1,14 @@
+package Models;
+
+public class Record {
+	
+	private int id;
+	private int price;
+	private String order_code;
+	private int count;
+	private int product_id;
+	private int seller_id;
+	private int customer_id;
+	private int shipping_id;
+
+}
