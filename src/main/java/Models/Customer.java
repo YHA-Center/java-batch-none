ee4d087f57155e1192ea557cb51035d03689810e
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-02-06 11:58:10 +0630

    done customer data access object

--- src/main/java/Models/Customer.java
+++ src/main/java/Models/Customer.java
@@ -9,7 +9,7 @@ public class Customer {
 	private String password;
 	private String phone;
 	private String image;
-	
+	private String address;
 	
 	// and then generated getter and setter 
 	// methods in order to manipulate with database
@@ -19,35 +19,47 @@ public class Customer {
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	public String getPhone() {
 		return phone;
 	}
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+	
 	public String getImage() {
 		return image;
 	}
 	public void setImage(String image) {
 		this.image = image;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
 	}
 	
 	
