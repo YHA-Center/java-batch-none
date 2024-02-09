3f8ff2a82033a55ab654cde99f383170f5bd0242
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-02-09 19:40:49 +0630

    from lucas

--- src/main/java/Controllers/AdminController.java
+++ src/main/java/Controllers/AdminController.java
@@ -29,7 +29,7 @@ public class AdminController extends HttpServlet {
         adminDAO = new AdminDAO();
     }
 
-
+    // post method
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		// get the action type
